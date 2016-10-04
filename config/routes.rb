3c33cea766ea2cce8e3b5cbcdee314851017d090
David Lejeune <Lejeune.David@outlook.com>
2016-10-04 10:17:12 +0200

    about file

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  # Ctrl shift D for duplicating lines
+  get 'static_pages/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
 end
