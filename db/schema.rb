a24274930f35e04ef3e4ddf09d8a0e1eff9146b3
David Lejeune <Lejeune.David@outlook.com>
2016-10-11 11:23:41 +0200

    add_index :users, :email, unique: true

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011071348) do
+ActiveRecord::Schema.define(version: 20161011092017) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
