4c2bee5ff5e4a0d62c6f4cb71b54087e9d21906e
David Lejeune <Lejeune.David@outlook.com>
2016-10-11 10:25:11 +0200

    bulk email test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,6 +9,16 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: "Example User", email: "user@example.com")
   end
 
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+
   test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
@@ -18,5 +28,15 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
+
+  test "email validation should accept valid addresses" do
+  valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                       first.last@foo.jp alice+bob@baz.cn]
+  valid_addresses.each do |valid_address|
+    @user.email = valid_address
+    assert @user.valid?, "#{valid_address.inspect} should be valid"
+  end
+end
+
 
 end
